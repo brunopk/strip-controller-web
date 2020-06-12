097681112788eb497dfbfb0dd3f61974a2a911e9
Bruno Piaggio <brunopiaggiok@gmail.com>
2020-06-11 23:43:10 -0300

    Login page (only static content)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    document.body.classList.add('text-center');
+
+    return (
+      <form className="form-signin">
+          <img className="mb-4" src="/logo72.png" alt="" width="72" height="72"/>
+              <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+              <label htmlFor="inputEmail" className="sr-only">Username</label>
+              <input type="email" id="inputEmail" className="form-control" placeholder="Username" required
+                     autoFocus/>
+                  <label htmlFor="inputPassword" className="sr-only">Password</label>
+                  <input type="password" id="inputPassword" className="form-control" placeholder="Password" required/>
+                  <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
   );
 }
 
