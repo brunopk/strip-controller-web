f1d082aaa8e7f7bc277a23746107211249da80a6
Bruno Piaggio <brunopiaggiok@gmail.com>
2020-06-12 00:19:37 -0300

    Implement routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
+import {Switch, Route} from 'react-router-dom'
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import './Login'
+import Login from "./Login";
 
 function App() {
 
     document.body.classList.add('text-center');
 
     return (
-      <form className="form-signin">
-          <img className="mb-4" src="/logo72.png" alt="" width="72" height="72"/>
-              <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-              <label htmlFor="inputEmail" className="sr-only">Username</label>
-              <input type="email" id="inputEmail" className="form-control" placeholder="Username" required
-                     autoFocus/>
-                  <label htmlFor="inputPassword" className="sr-only">Password</label>
-                  <input type="password" id="inputPassword" className="form-control" placeholder="Password" required/>
-                  <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      </form>
+        <div>
+            <Switch>
+                <Route path="/login">
+                    <Login />
+                </Route>
+
+                {/* Note how these two routes are ordered. The more specific
+            path="/contact/:id" comes before path="/contact" so that
+            route will render when viewing an individual contact
+                <Route path="/contact/:id">
+                    <Contact />
+                </Route>
+                <Route path="/contact">
+                    <AllContacts />
+                </Route>*/
+
+                /* If none of the previous routes render anything,
+            this route acts as a fallback.
+
+            Important: A route with path="/" will *always* match
+            the URL because all URLs begin with a /. So that's
+            why we put this one last of all */}
+                <Route path="/">
+                    <Login />
+                </Route>
+            </Switch>
+        </div>
   );
 }
 
