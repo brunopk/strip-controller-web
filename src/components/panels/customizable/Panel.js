18d6e39188ba559e8adee8d386e8df923a1c70b0
Bruno Piaggio <brunopiaggiok@gmail.com>
2020-07-05 21:48:05 -0300

    Improve Panel component on src/components/dashboard/panels/customizable

--- src/components/panels/customizable/Panel.js
+++ src/components/panels/customizable/Panel.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import {Panel as Colors} from "../colors"
+import { Range } from "rc-slider";
 import "./Panel.css"
-import * as Icon from 'react-feather';
-import { Range, Handle } from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import _ from "lodash"
+import * as Icon from "react-feather";
+
 
 class ControlledRange extends React.Component {
   constructor(props) {
@@ -24,22 +26,21 @@ class ControlledRange extends React.Component {
   }
 }
 
-function Section({title}){
-
+function SectionColorConfiguration({title}){
   return (
-    <div class="card">
-      <div class="card-header" id="headingTwo">
-        <h5 class="mb-0">
-          <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+    <div className="card">
+      <div className="card-header" id="headingTwo">
+        <h5 className="mb-0">
+          <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
             {title}
           </button>
-          <button class="btn btn-danger section-btn">
+          <button className="btn btn-danger section-btn">
             <Icon.X/>
           </button>
         </h5>
       </div>
-      <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo">
-        <div class="card-body">
+      <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo">
+        <div className="card-body">
           <Colors editable={true} />
         </div>
       </div>
@@ -47,105 +48,119 @@ function Section({title}){
   );
 }
 
-function RangeConfig(){
+function SectionRangeConfiguration({title}){
 
   return (
     <div className="row row-with-margin-top align-items-center">
-      <div className="col col-3">
-        <span>Section 1</span>
+      <div className="col col-4">
+        <span>{title}</span>
       </div>
-      <div className="col col-2">
+      <div className="col col-2 text-right">
         <span>From:</span>
       </div>
       <div className="col col-2">
-        <input type="number" class="form-control"/>
+        <input type="number" className="form-control"/>
       </div>
-      <div className="col col-2">
+      <div className="col col-2 text-right">
         <span>To:</span>
       </div>
       <div className="col col-2">
-        <input type="number" class="form-control"/>
+        <input type="number" className="form-control"/>
       </div>
     </div>
   )
 }
 
-function Panel({values, onChose, editable }) {
+function Panel() {
+
+    const [numberOfSections, setNumberOfSections] = useState({actual: 1, prev: 1});
+    const [numberOfLeds, setNumberOfLeds] = useState({actual: 300, prev: 300});
+    const handleChangeNumberOfSections = (prevNumberOfSections) => (event) => {
+      setNumberOfSections({
+        actual: event.target.value,
+        prev: prevNumberOfSections
+      })
+    }
+    const handleChangeNumberOfLeds = (prevNumberOfLeds) => (event) => {
+      setNumberOfLeds({
+        actual: event.target.value,
+        prev: prevNumberOfLeds
+      })
+    }
 
     return (
       <div className="container-fluid panel">
         <div className="row">
           <div className="col  col-12">
             <div id="accordion">
-              <div class="card">
-                <div class="card-header" id="headingOne">
-                  <h5 class="mb-0">
-                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseShape" aria-expanded="false" aria-controls="collapseShape">
+              <div className="card">
+                <div className="card-header" id="headingOne">
+                  <h5 className="mb-0">
+                    <button className="btn btn-link" data-toggle="collapse" data-target="#collapseShape" aria-expanded="false" aria-controls="collapseShape">
                       <span>
                         <Icon.Codesandbox style={{marginRight: "5px"}}/>Shape 
                       </span>  
                     </button>
-                </h5>
-              </div>
-              <div id="collapseShape" class="collapse show" aria-labelledby="headingOne">
-                <div class="card-body">
-                  <div className="container-fluid">
-                    <div className="row">
-                      <div className="col col-12">
-                        <div class="list-group">
-                          <button class="list-group-item list-group-item-action list-group-item-primary">Squares and lines</button>
+                  </h5>
+                </div>
+                <div id="collapseShape" className="collapse show" aria-labelledby="headingOne">
+                  <div className="card-body">
+                    <div className="container-fluid">
+                      <div className="row">
+                        <div className="col col-12">
+                          <div className="list-group">
+                            <button className="list-group-item list-group-item-action list-group-item-primary">Squares and lines</button>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                    <div className="row">
-                      <div className="col col-12">
-                        <div class="list-group">
-                          <button class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">Cylinder</button>
+                      <div className="row">
+                        <div className="col col-12">
+                          <div className="list-group">
+                            <button className="list-group-item list-group-item-action disabled" tabIndex={-1} aria-disabled="true">Cylinder</button>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                    <div className="row row-with-margin-top align-items-center">
-                      <div className="col col-3">
-                        <span>Number of leds:</span>
-                      </div>
-                      <div className="col col-8">
-                        <input type="number" class="form-control"/>
-                      </div>
-                      <div className="col col-1">
-                        <button class="btn btn-primary section-btn">
-                          <Icon.RefreshCw/>
-                        </button>
-                      </div>
-                    </div>
-                    <div className="row row-with-margin-top align-items-center">
-                      <div className="col col-3">
-                        <span>Number of sections:</span>
-                      </div>
-                      <div className="col col-8">
-                        <input type="number" class="form-control"/>
-                      </div>
-                      <div className="col col-1">
-                        <button class="btn btn-primary section-btn">
-                          <Icon.RefreshCw/>
-                        </button>
-                      </div>
-                    </div>
-                    <div className="row row-with-margin-top align-items-center">
-                      <div className="col col-3">
-                        <span>Sections:</span>
-                      </div>
-                      <div className="col col-9">
-                        <div className="form-control form-control-slider">
-                          <ControlledRange />
+                      <div className="row row-with-margin-top align-items-center">
+                        <div className="col col-4">
+                          <span>Number of leds:</span>
+                        </div>
+                        <div className="col col-8">
+                          <input 
+                            type="number" 
+                            value={numberOfLeds.actual} 
+                            className="form-control"
+                            onChange={handleChangeNumberOfLeds(numberOfLeds.actual)}
+                          />
                         </div>
                       </div>
+                      <div className="row row-with-margin-top align-items-center">
+                        <div className="col col-4">
+                          <span>Number of sections:</span>
+                        </div>
+                        <div className="col col-8">
+                          <input 
+                            type="number" 
+                            className="form-control" 
+                            value={numberOfSections.actual} 
+                            onChange={handleChangeNumberOfSections(numberOfSections.actual)}
+                          />
+                        </div>
+                      </div>
+                      <div className="row row-with-margin-top align-items-center">
+                        <div className="col col-4">
+                          <span>Sections:</span>
+                        </div>
+                        <div className="col col-8">
+                          <div className="form-control form-control-slider">
+                            <ControlledRange key={1} />
+                          </div>
+                        </div>
+                      </div>
+                      {_.times(numberOfSections.actual, (i) => <SectionRangeConfiguration key={i+1} title={`Section ${i+1}`}/>)}
                     </div>
-                    <RangeConfig/>
                   </div>
                 </div>
               </div>
-            </div>
-            <Section title="Section #1"/>
           </div>
         </div>
       </div>
