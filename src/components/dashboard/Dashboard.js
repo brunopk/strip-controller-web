e550135c8980c653f610162e49a1eca961b62806
Bruno Piaggio <brunopiaggiok@gmail.com>
2021-01-09 22:33:42 -0300

    Improve header style

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -16,31 +16,24 @@ function Dashboard({CurrentPanel}) {
 
     return (
         <div className="dashboard">
+          <footer class="footer">
+      <div class="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+      </div>
+    </footer>
             <nav className="navbar navbar-expand-lg navbar-light sticky-top bg-primary flex-md-nowrap p-0 shadow">
-                <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="/strip-controller"><Logo/></a>
-                <button className="navbar-toggler position-absolute d-md-none collapsed" type="button"
+                <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3 pt-2 pb-2" href="/strip-controller"><Logo/></a>
+                <button className="navbar-toggler position-absolute d-md-none collapsed hidden-md-up" type="button"
                         data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
                         aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"/>
                 </button>
                 <input className="form-control form-control-dark w-100 bg-primary" type="text" readOnly={true}/>
-                    <ul className="navbar-nav px-3">
-                        <li className="nav-item text-nowrap">
-                            <a className="nav-link" href="/logut">
-                                <button className="btn btn-success">< Icon.PauseCircle /></button>
-                            </a>
-                        </li>
-                        <li className="nav-item text-nowrap">
-                            <a className="nav-link" href="/logut">
-                                <button className="btn btn-danger">< Icon.Power /></button>
-                            </a>
-                        </li>
-                    </ul>
             </nav>
             <div className="container-fluid">
                 <div className="row">
                     <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-                        <div className="sidebar-sticky pt-3">
+                        <div className="sidebar-sticky pt-3 pb-3">
                             <ul className="nav flex-column">
                                 <li className="nav-item">
                                     <a className={"nav-link " + (CurrentPanel === PColors ? "active" : "")} href="/dash">
