0366935e5735c5ec2ce8c8069a2769d25fa42f90
Bruno Piaggio <brunopiaggiok@gmail.com>
2021-01-13 23:16:50 -0300

    Remove commented code and console.log

--- src/components/dashboard/MobileButtonMenu.js
+++ src/components/dashboard/MobileButtonMenu.js
@@ -6,8 +6,6 @@ function MobileButtonMenu({ isLandscape }) {
   const className = isLandscape ? 'landscape-button-menu' : 'portrait-button-menu';
   const flex = isLandscape ? 'flex-column' : 'flex-row';
 
-  console.log(isLandscape);
-  
   return (
     <div className={className}>
       <ul className={`nav ${flex} bg-primary`}>
@@ -24,19 +22,5 @@ function MobileButtonMenu({ isLandscape }) {
     </div>
   );
 }
-
-/* <footer className="footer">
-      <ul className="nav flex-row button-menu-sm bg-primary">
-        { buttonList.map(({ Icon }, index) => (
-          <li className="nav-item text-nowrap" key={index}>
-            <a className="nav-link" href="/logut">
-              <button type="button" className="btn" title="Reset">
-                <Icon size={18} className="btn-white" />
-              </button>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </footer> */
 
 export default MobileButtonMenu;
