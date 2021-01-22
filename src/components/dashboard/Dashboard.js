1e13ff7906b6710d2ca3f4b2144ba4b8e2d2d882
Bruno Piaggio <brunopiaggiok@gmail.com>
2021-01-22 16:54:08 -0300

    Include ApiContext on Dashboard component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -7,9 +7,10 @@ import MainMenu from './MainMenu';
 import DesktopButtonMenu from './DesktopButtonMenu';
 import MobileButtonMenu from './MobileButtonMenu';
 import { setBodyClass, setRootClass } from '../../utils/css';
-import { ButtonMenuContextProvider } from '../../context/ButtonMenuContext';
+import { ButtonMenuContextProvider, ApiContextProvider } from '../../context';
 
 function Dashboard({ CurrentPanel, isLandscape, isPortrait }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [buttonMenu, setButtonMenu] = useState({
     interchangeable: typeof CurrentPanel.Buttons !== 'undefined' ? CurrentPanel.Buttons : [],
     fixed: [{
@@ -29,41 +30,43 @@ function Dashboard({ CurrentPanel, isLandscape, isPortrait }) {
   setBodyClass('body-dashboard');
 
   return (
-    <ButtonMenuContextProvider
-      buttonList={buttonMenu.interchangeable.concat(buttonMenu.fixed)}
-      setButtonList={setButtonList(buttonMenu)}>
-      <div className="dashboard">
-        <nav className="navbar navbar-expand-lg navbar-light sticky-top bg-primary flex-md-nowrap p-0 shadow">
-          <div className="col-md-3 col-lg-2 mr-0 px-3 pt-2 pb-2">
-            <Logo />
+    <ApiContextProvider isFetching={isFetching} setIsFetching={setIsFetching}>
+      <ButtonMenuContextProvider
+        buttonList={buttonMenu.interchangeable.concat(buttonMenu.fixed)}
+        setButtonList={setButtonList(buttonMenu)}>
+        <div className="dashboard">
+          <nav className="navbar navbar-expand-lg navbar-light sticky-top bg-primary flex-md-nowrap p-0 shadow">
+            <div className="col-md-3 col-lg-2 mr-0 px-3 pt-2 pb-2">
+              <Logo />
+            </div>
+            <button
+              className="navbar-toggler position-absolute d-md-none collapsed hidden-md-up"
+              type="button"
+              data-toggle="collapse"
+              data-target="#main-menu-sm"
+              aria-controls="main-menu-sm"
+              aria-expanded="false"
+              aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon" />
+            </button>
+            <input className="form-control form-control-dark w-100 bg-primary" type="text" readOnly />
+            {/* controlled with */}
+            <DesktopButtonMenu />
+            <MainMenu id="main-menu-sm" currentPanel={CurrentPanel} />
+          </nav>
+          <div className="container-fluid">
+            <div className="row">
+              <MainMenu id="main-menu-lg" currentPanel={CurrentPanel} />
+              <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-md-4 h-100">
+                {/* TODO color list should come from server TODO implement onChose */}
+                <CurrentPanel values={[]} editable onChose={(hex) => console.log(hex)} />
+              </main>
+            </div>
           </div>
-          <button
-            className="navbar-toggler position-absolute d-md-none collapsed hidden-md-up"
-            type="button"
-            data-toggle="collapse"
-            data-target="#main-menu-sm"
-            aria-controls="main-menu-sm"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon" />
-          </button>
-          <input className="form-control form-control-dark w-100 bg-primary" type="text" readOnly />
-          {/* controlled with */}
-          <DesktopButtonMenu />
-          <MainMenu id="main-menu-sm" currentPanel={CurrentPanel} />
-        </nav>
-        <div className="container-fluid">
-          <div className="row">
-            <MainMenu id="main-menu-lg" currentPanel={CurrentPanel} />
-            <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-md-4 h-100">
-              {/* TODO color list should come from server TODO implement onChose */}
-              <CurrentPanel values={[]} editable onChose={(hex) => console.log(hex)} />
-            </main>
-          </div>
+          {isPortrait ? <MobileButtonMenu isLandscape={isLandscape} /> : <></>}
         </div>
-        {isPortrait ? <MobileButtonMenu isLandscape={isLandscape} /> : <></>}
-      </div>
-    </ButtonMenuContextProvider>
+      </ButtonMenuContextProvider>
+    </ApiContextProvider>
   );
 }
 
