285c4a7de015da3b8906d26f9acd0b2c7e973d97
Bruno Piaggio <brunopiaggiok@gmail.com>
2021-01-25 00:24:26 -0300

    Add TODO

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 function Form({ id, children }) {
+  // TODO: add <input type="submit" /> (and replace <form> by <Form> in Modal component)
   return (
     <form id={id} className="w-100">
       {children}
