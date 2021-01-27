09e34da7a2817fdc69ff46403cff3467dc1d9ed4
Bruno Piaggio <brunopiaggiok@gmail.com>
2021-01-27 18:11:02 -0300

    Fix Login to validate unfilled fields

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -4,28 +4,38 @@ import { UserContext } from '../../context/UserContext';
 import { setBodyClass, setRootClass } from '../../utils/css';
 import Logo from '../logo';
 import Loader from '../loader';
+import $ from 'jquery';
 import './Login.css';
 
 function Login() {
   const [isFetching] = useState(false);
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
   const { setToken } = useContext(UserContext);
   const history = useHistory();
   const location = useLocation();
   const { from } = location.state || { from: { pathname: '' } };
   const login = async () => {
-    try {
-      // const resp = await getToken();
-      // if (resp.ok) {
-      // const body = await resp.json()
-      setToken('asds');
-      // Redirect
-      history.replace(from);
-      // } else {
-      // TODO
-      // }
-    } catch (error) {
-      // TODO
-      console.log(error);
+    const form = $('#form-login');
+    if (!form[0].checkValidity()) {
+      // If the form is invalid, submit it. The form won't actually submit;
+      // this will just cause the browser to display the native HTML5 error messages.
+      form.find(':submit').trigger('click');
+    } else {
+      try {
+        // const resp = await getToken();
+        // if (resp.ok) {
+        // const body = await resp.json()
+        setToken('asds');
+        // Redirect
+        history.replace(from);
+        // } else {
+        // TODO
+        // }
+      } catch (error) {
+        // TODO
+        console.log(error);
+      }
     }
   };
 
@@ -39,15 +49,31 @@ function Login() {
   return isFetching ? (
     <Loader />
   ) : (
-    <div className="form-signin">
-      <img className="mb-4" src="/logo72.png" alt="" width="72" height="72" />
-      <Logo />
-      <label htmlFor="inputEmail" className="sr-only">Username</label>
-      <input id="inputEmail" className="form-control" placeholder="Username" required />
-      <label htmlFor="inputPassword" className="sr-only">Password</label>
-      <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-      <button className="btn btn-lg btn-primary btn-block" onClick={login}>Sign in</button>
-    </div>
+    <form id="form-login">
+      <div className="form-signin">
+        <img className="mb-4" src="/logo72.png" alt="" width="72" height="72" />
+        <Logo />
+        <label htmlFor="inputEmail" className="sr-only">Username</label>
+        <input
+          id="inputEmail"
+          className="form-control"
+          placeholder="Username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+          required />
+        <label htmlFor="inputPassword" className="sr-only">Password</label>
+        <input
+          id="inputPassword"
+          type="password"
+          className="form-control"
+          value={password}
+          placeholder="Password"
+          onChange={(event) => setPassword(event.target.value)}
+          required />
+        <button className="btn btn-lg btn-primary btn-block" onClick={login}>Sign in</button>
+        <input type="submit" />
+      </div>
+    </form>
   );
 }
 
