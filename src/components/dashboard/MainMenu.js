719d57be44f705be61639510dcce214726dd653a
Bruno Piaggio <brunopiaggiok@gmail.com>
2021-01-28 02:10:42 -0300

    Fix MainMenu to collapse on mobile after click

--- src/components/dashboard/MainMenu.js
+++ src/components/dashboard/MainMenu.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as Icon from 'react-feather';
+import $ from 'jquery';
 import PColors from '../panels/colors/PColors';
 import PCustomization from '../panels/customization/PCustomization';
 import PEffects from '../panels/effects/PEffects';
 
 function MainMenu({ id, currentPanel }) {
+  const collapseMenu = () => $('.navbar-toggler').trigger('click');
   return (
     <nav id={id} className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
       <div className="sidebar-sticky pt-3 pb-3">
         <ul className="nav flex-column">
           <li className="nav-item">
-            <Link className={`nav-link ${currentPanel === PColors ? 'active' : ''}`} to="/dash">
+            <Link
+              className={`nav-link ${currentPanel === PColors ? 'active' : ''}`}
+              to="/dashboard"
+              onClick={() => collapseMenu()}>
               <Icon.Grid className="feather" />
               Colors
               <span className="sr-only" />
             </Link>
           </li>
           <li className="nav-item">
-            <Link className={`nav-link ${currentPanel === PEffects ? 'active' : ''}`} to="/effects">
+            <Link
+              className={`nav-link ${currentPanel === PEffects ? 'active' : ''}`}
+              to="/effects"
+              onClick={() => collapseMenu()}>
               <Icon.Star className="feather" />
               Effects
               <span className="sr-only" />
             </Link>
           </li>
           <li className="nav-item">
-            <Link className={`nav-link ${currentPanel === PCustomization ? 'active' : ''}`} to="/customizable">
+            <Link
+              className={`nav-link ${currentPanel === PCustomization ? 'active' : ''}`}
+              to="/customizable"
+              onClick={() => collapseMenu()}>
               <Icon.Sliders className="feather" />
               Customization
               <span className="sr-only" />
