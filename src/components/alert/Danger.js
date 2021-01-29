567f3ef143ade5b6a491fd02d330a712c2d0930f
Bruno Piaggio <brunopiaggiok@gmail.com>
2021-01-29 13:08:58 -0300

    Add className property to Danger

--- src/components/alert/Danger.js
+++ src/components/alert/Danger.js
@@ -3,9 +3,9 @@ import React from 'react';
 /**
  * Wrapper for Bootstrap alert (see https://getbootstrap.com/docs/4.0/components/alerts/)
  */
-function Danger({ children }) {
+function Danger({ children, className }) {
   return (
-    <div className="alert alert-danger" role="alert">
+    <div className={`alert alert-danger ${className}`} role="alert">
       {children}
     </div>
   );
