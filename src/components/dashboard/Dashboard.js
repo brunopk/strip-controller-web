3d51eaa4dbeac0afafe60c822faf87e99ec3b6f1
Bruno Piaggio <brunopiaggiok@gmail.com>
2021-02-05 18:19:34 -0300

    Add back button for Dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -5,10 +5,12 @@ import * as Icon from 'react-feather';
 import MainMenu from './MainMenu';
 import DesktopButtonMenu from './DesktopButtonMenu';
 import MobileButtonMenu from './MobileButtonMenu';
+import { useHistory } from 'react-router-dom';
 import { setBodyClass, setRootClass } from '../../utils/css';
 import { ButtonMenuContextProvider } from '../../context';
 
 function Dashboard({ CurrentPanel, isLandscape, isPortrait }) {
+  const history = useHistory();
   const [contextualButtonMenu, setButtonContextualMenu] = useState({
     interchangeable: typeof CurrentPanel.Buttons !== 'undefined' ? CurrentPanel.Buttons : [],
     fixed: [{
@@ -37,6 +39,9 @@ function Dashboard({ CurrentPanel, isLandscape, isPortrait }) {
       }>
       <div className="dashboard">
         <nav className="navbar navbar-expand-lg navbar-light sticky-top bg-primary flex-md-nowrap p-0 shadow">
+          <div className="col-1 pt-2 pb-2">
+            <Icon.ArrowLeft size={18} className="color-white" onClick={() => history.goBack()} />
+          </div>
           <button
             className="navbar-toggler position-absolute d-md-none collapsed hidden-md-up"
             type="button"
@@ -47,7 +52,7 @@ function Dashboard({ CurrentPanel, isLandscape, isPortrait }) {
             aria-label="Toggle navigation">
             <Icon.Menu className="color-white" />
           </button>
-          <input className="form-control form-control-dark w-100 bg-primary" type="text" readOnly />
+          <input className="form-control form-control-dark w-100 bg-primary d-lg-block d-none" type="text" readOnly />
           {/* controlled with */}
           <DesktopButtonMenu />
           <MainMenu id="main-menu-sm" currentPanel={CurrentPanel} isLandscape={isLandscape} />
