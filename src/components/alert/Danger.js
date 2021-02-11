1a7acc050dfca6a9c9d582a4cd84ab67628e43d6
Bruno Piaggio <brunopiaggiok@gmail.com>
2021-02-11 16:33:22 -0300

    Fix f725703c841a097f3116aec6b4d259022196934d

--- src/components/alert/Danger.js
+++ src/components/alert/Danger.js
@@ -3,9 +3,10 @@ import React from 'react';
 /**
  * Wrapper for Bootstrap alert (see https://getbootstrap.com/docs/4.0/components/alerts/)
  */
-function Danger({ children }) {
+function Danger({ children, className }) {
+  className = typeof className !== 'undefined' ? className : '';
   return (
-    <div className="alert alert-danger text-danger" role="alert">
+    <div className={`alert alert-danger text-danger ${className}`} role="alert">
       {children}
     </div>
   );
